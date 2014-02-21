2b74e0e94ec552b2835015e96049d06eb920bd2b
Kévin PETIT <kevin.petit@arm.com>
2014-02-21 10:53:49 +0000

    AArch64: Add support for arm64
    Change-Id: I38d24415a21179093e31d7c711e1d0e7274adc24
    Signed-off-by: Marcus Oakland <marcus.oakland@arm.com>
    Signed-off-by: Kévin PETIT <kevin.petit@arm.com>

--- build/build_config.h
+++ build/build_config.h
@@ -117,6 +117,11 @@
 #define ARCH_CPU_ARMEL 1
 #define ARCH_CPU_32_BITS 1
 #define WCHAR_T_IS_UNSIGNED 1
+#elif defined(__aarch64__)
+#define ARCH_CPU_ARM64_FAMILY 1
+#define ARCH_CPU_ARM64EL 1
+#define ARCH_CPU_64_BITS 1
+#define WCHAR_T_IS_UNSIGNED 1
 #elif defined(__MIPSEL__)
 #define ARCH_CPU_MIPS_FAMILY 1
 #define ARCH_CPU_MIPSEL 1
